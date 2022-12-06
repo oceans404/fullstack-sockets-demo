473bdc50b14f3e76af5e42390c661fb2bfa223a4
Steph <steph@oceans404.com>
2022-12-06 13:01:19 -0800

    working starter socket chat frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,56 @@
+import { useState, useRef, useEffect } from "react";
+import "./App.css";
+import { SingleFieldForm } from "./SingleFieldForm";
+import { Container } from "@chakra-ui/react";
+
+function App() {
+  const ref = useRef(null);
+  const [userName, setUserName] = useState(null);
+  const [chats, setChats] = useState([]);
+
+  const handleNewMessage = (vals) => {
+    setChats([...chats, vals]);
+  };
+
+  useEffect(() => {
+    scrollToNewChat();
+  }, [chats]);
+
+  const scrollToNewChat = () => {
+    // console.log(ref);
+    if (ref.current) {
+      ref.current.scrollTop = ref.current.scrollHeight;
+    }
+  };
+
+  return (
+    <div className="App">
+      {!!userName ? (
+        <Container w="90vw">
+          <h1>{!!userName && `gm ${userName}`}</h1>
+          <ul id="chat-scroll" ref={ref}>
+            {chats.map((c) => (
+              <li k={c.Message} className="chat-message">
+                {userName}: {c.Message}
+              </li>
+            ))}
+          </ul>
+          <SingleFieldForm
+            getFormValues={handleNewMessage}
+            formField="Message"
+            buttonText="Send"
+          />
+        </Container>
+      ) : (
+        <SingleFieldForm
+          fullWidth
+          getFormValues={(vals) => setUserName(vals.Username)}
+          formField="Username"
+          buttonText="Chat"
+        />
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/SingleFieldForm.jsx
@@ -0,0 +1,37 @@
+import { useForm } from "react-hook-form";
+import { Input, Button } from "@chakra-ui/react";
+
+export function SingleFieldForm({
+  getFormValues,
+  formField,
+  buttonText,
+  fullWidth,
+}) {
+  const {
+    handleSubmit,
+    register,
+    reset,
+    formState: { isSubmitting },
+  } = useForm();
+
+  function onSubmit(values) {
+    getFormValues(values);
+    reset();
+  }
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Input
+        p={10}
+        type="text"
+        placeholder={formField}
+        {...register(formField, { required: true })}
+        w="100%"
+      />
+
+      <Button colorScheme="teal" isLoading={isSubmitting} type="submit">
+        {buttonText}
+      </Button>
+    </form>
+  );
+}
